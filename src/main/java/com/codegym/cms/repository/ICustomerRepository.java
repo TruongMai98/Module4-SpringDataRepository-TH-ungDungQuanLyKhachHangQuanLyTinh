76f7d8bc6dc9ba71e41763ec691f00e39110f183
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-12-20 15:42:11 +0700

    [Thực hành] Ứng dụng Quản lý khách hàng: Quản lý tỉnh

---
+++ src/main/java/com/codegym/cms/repository/ICustomerRepository.java
@@ -0,0 +1,13 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.web.PageableArgumentResolver;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+    Iterable<Customer> findAllByProvince(Province province);
+}
